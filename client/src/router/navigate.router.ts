ba68ccf0bf933ab96e706a8cc848840a9fe1be81
arfath-ali <1290sfaa28@gmail.com>
2026-04-29 09:49:04 +0530

    feat: use Partial<Record> for AppState title map add new route called dicover and implement prettier

--- client/src/router/navigate.router.ts
+++ client/src/router/navigate.router.ts
@@ -1,25 +1,31 @@
-import { setAppState } from '../state/app.state.js';
-import type { AppState } from '../types/app-state.types.js';
+import { setAppState } from "../state/app.state.js";
+import type { AppState } from "../types/app-state.types.js";
 
 export function navigate() {
   let route = location.pathname.slice(1) as AppState;
 
-  if (route === '') {
-    history.replaceState({}, '', '/home');
-    route = 'home';
+  if (route === "") {
+    history.replaceState({}, "", "/home");
+    route = "home";
+  }
+
+  if (route === "discover") {
+    history.replaceState({}, "", "/movies");
+    route = "movies";
   }
 
   const validAppStates: AppState[] = [
-    'home',
-    'movies',
-    'tv-shows',
-    'details',
-    'search',
-    'watchlist',
-    'profile',
+    "home",
+    "discover",
+    "movies",
+    "tv-shows",
+    "details",
+    "search",
+    "watchlist",
+    "profile",
   ];
 
   if (validAppStates.includes(route)) {
     setAppState(route);
-  } else setAppState('error');
+  } else setAppState("error");
 }
--- client/src/utils/meta-data.utils.ts
+++ client/src/utils/meta-data.utils.ts
@@ -1,17 +1,17 @@
-import type { AppState } from '../types/app-state.types.js';
+import type { AppState } from "../types/app-state.types.js";
 
-const TITLES: Record<AppState, string> = {
-  '': 'Picturehouse',
-  home: 'Picturehouse | Discover Cinema',
-  movies: 'Movies | Picturehouse',
-  'tv-shows': 'TV Shows | Picturehouse',
-  details: 'Details | Picturehouse',
-  search: 'Search | Picturehouse',
-  watchlist: 'My Watchlist | Picturehouse',
-  profile: 'My Profile | Picturehouse',
-  error: 'Page not found | Picturehouse',
+const TITLES: Partial<Record<AppState, string>> = {
+  "": "Picturehouse",
+  home: "Picturehouse | Discover Cinema",
+  movies: "Movies | Picturehouse",
+  "tv-shows": "TV Shows | Picturehouse",
+  details: "Details | Picturehouse",
+  search: "Search | Picturehouse",
+  watchlist: "My Watchlist | Picturehouse",
+  profile: "My Profile | Picturehouse",
+  error: "Page not found | Picturehouse",
 };
 
 export function updatePageTitle(title: AppState) {
-  document.title = TITLES[title];
+  document.title = TITLES[title] ?? "Picturehouse";
 }
